6c68d74548bc00f7b9194db8162dc33fc12a4796
Georg Wallisch <gw@phpco.de>
2023-06-24 20:11:59 +0200

    sql init

--- init_db.sql
+++ init_db.sql
@@ -1,59 +1,55 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
+DROP TABLE IF EXISTS `artikel`;
 CREATE TABLE `artikel` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `pzn` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `df` varchar(5) DEFAULT NULL,
-  `pm` varchar(20) DEFAULT NULL,
-  `pe` varchar(10) DEFAULT NULL,
-  `hersteller` varchar(8) DEFAULT NULL,
-  `ek` decimal(6,2) DEFAULT NULL,
-  `vk` decimal(6,2) DEFAULT NULL,
-  `AM` tinyint(1) DEFAULT NULL,
-  `DC` tinyint(1) DEFAULT NULL,
-  `MP` tinyint(1) DEFAULT NULL,
-  `LM` tinyint(1) DEFAULT NULL,
-  `KM` tinyint(1) DEFAULT NULL,
-  `ABDATA` tinyint(1) DEFAULT NULL,
-  `dirty` tinyint(1) DEFAULT NULL,
-  `stand` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pzn` (`pzn`)
-) ENGINE=InnoDB AUTO_INCREMENT=6317 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci	
+  `id` bigint(20) NOT NULL,
+  `pzn` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `df` varchar(5) DEFAULT NULL,
+  `pm` varchar(20) DEFAULT NULL,
+  `pe` varchar(10) DEFAULT NULL,
+  `hersteller` varchar(8) DEFAULT NULL,
+  `ek` decimal(6,2) DEFAULT NULL,
+  `vk` decimal(6,2) DEFAULT NULL,
+  `AM` tinyint(1) DEFAULT NULL,
+  `DC` tinyint(1) DEFAULT NULL,
+  `MP` tinyint(1) DEFAULT NULL,
+  `LM` tinyint(1) DEFAULT NULL,
+  `KM` tinyint(1) DEFAULT NULL,
+  `ABDATA` tinyint(1) DEFAULT NULL,
+  `dirty` tinyint(1) DEFAULT NULL,
+  `stand` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `preise`;
 CREATE TABLE `preise` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `artikel_id` bigint(20) NOT NULL,
-  `standort_id` int(11) NOT NULL,
-  `eek` decimal(6,2) DEFAULT NULL,
-  `evk` decimal(6,2) DEFAULT NULL,
-  `avk` decimal(6,2) DEFAULT NULL,
-  `lager` tinyint(1) DEFAULT NULL,
-  `bestand` smallint(5) unsigned DEFAULT NULL,
-  `preisaktion` varchar(50) DEFAULT NULL,
-  `ap` decimal(6,2) DEFAULT NULL,
-  `kalkulationsmodell` varchar(100) DEFAULT NULL,
-  `dirty` tinyint(1) DEFAULT NULL,
-  `stand` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `artikel_id` (`artikel_id`),
-  KEY `standort_id` (`standort_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10513 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+  `id` bigint(20) NOT NULL,
+  `artikel_id` bigint(20) NOT NULL,
+  `standort_id` int(11) NOT NULL,
+  `eek` decimal(6,2) DEFAULT NULL,
+  `evk` decimal(6,2) DEFAULT NULL,
+  `avk` decimal(6,2) DEFAULT NULL,
+  `lager` tinyint(1) DEFAULT NULL,
+  `bestand` smallint(5) UNSIGNED DEFAULT NULL,
+  `preisaktion` varchar(50) DEFAULT NULL,
+  `ap` decimal(6,2) DEFAULT NULL,
+  `kalkulationsmodell` varchar(100) DEFAULT NULL,
+  `dirty` tinyint(1) DEFAULT NULL,
+  `stand` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `pzns`;
 CREATE TABLE `pzns` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `artikel_id` bigint(20) NOT NULL,
-  `standort_id` int(11) DEFAULT NULL,
-  `pzn` int(11) DEFAULT NULL,
-  `dirty` tinyint(1) DEFAULT NULL,
-  `stand` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `artikel_id` (`artikel_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci	
+  `id` bigint(20) NOT NULL,
+  `artikel_id` bigint(20) NOT NULL,
+  `standort_id` int(11) DEFAULT NULL,
+  `pzn` int(11) DEFAULT NULL,
+  `dirty` tinyint(1) DEFAULT NULL,
+  `stand` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+DROP TABLE IF EXISTS `standorte`;
 CREATE TABLE `standorte` (
   `id` int(11) NOT NULL,
   `idf` int(11) NOT NULL,
@@ -67,10 +63,31 @@ INSERT INTO `standorte` (`id`, `idf`, `name`, `kurz`) VALUES
 (3, 4549705, 'Windischeschenbach', 'WESB'),
 (4, 4517740, 'Weiden', 'AEWEN');
 
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pzn` (`pzn`);
+
+ALTER TABLE `preise`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artikel_id` (`artikel_id`),
+  ADD KEY `standort_id` (`standort_id`);
+
+ALTER TABLE `pzns`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artikel_id` (`artikel_id`);
+
 ALTER TABLE `standorte`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `idf` (`idf`);
 
+ALTER TABLE `artikel`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `preise`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `pzns`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `standorte`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
